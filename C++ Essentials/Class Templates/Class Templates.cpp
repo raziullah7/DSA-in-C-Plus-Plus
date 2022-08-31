0c61a841a0f8775578a90f70c58400499e6805f0
Razi Ullah <raziullah913@gmail.com>
2022-08-31 20:36:05 +0500

    class template (update)

--- C++ Essentials/Class Templates/Class Templates.cpp
+++ C++ Essentials/Class Templates/Class Templates.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    MyPair<int> somePair;
-    cout << somePair.getMax(3, 79) << endl;
+    MyPair<int> somePair(3, 79);
+    cout << somePair.getMax() << endl;
 
-    MyPair<float> anotherPair;
-    cout << somePair.getMax(3.29, 3.79) << endl;
+    MyPair<float> anotherPair(3.29, 3.79);
+    cout << anotherPair.getMax() << endl;
 
     return 1;
 }
--- C++ Essentials/Class Templates/MyPair.h
+++ C++ Essentials/Class Templates/MyPair.h
@@ -3,10 +3,18 @@ template <class T>
 
 class MyPair {
 private:
+	// fields
 	T a, b;
 
 public:
-	T getMax(T a, T b) {
+	// parameterized constructor
+	MyPair(T a, T b) {
+		this->a = a;
+		this->b = b;
+	}
+
+	// method to get max values of fields
+	T getMax() {
 		return (a > b ? a : b);
 	};
 };
