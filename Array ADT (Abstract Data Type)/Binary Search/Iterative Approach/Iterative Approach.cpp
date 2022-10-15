afb40d9d8a84ac0e8afbac98677d8808873384b2
Razi Ullah <raziullah913@gmail.com>
2022-10-15 19:57:29 +0500

    update

--- Array ADT (Abstract Data Type)/Binary Search/Iterative Approach/Iterative Approach.cpp
+++ Array ADT (Abstract Data Type)/Binary Search/Iterative Approach/Iterative Approach.cpp
@@ -6,6 +6,12 @@ class IterativeBinarySearch {
 private:
     T* Arr;
     int n;
+    void Display(int value) {
+        if (value == -1)
+            cout << "Key Not Found\n";
+        else
+            cout << "Key Found on Index " << value << endl;
+    }
 
 public:
     IterativeBinarySearch(int size) {
@@ -19,17 +25,26 @@ public:
     int BinarySearch(int low, int high, T key) {
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (key == Arr[mid])
-                return mid;
+            if (key == Arr[mid]) {
+                Display(mid);
+                return mid; // successful
+            }
             else if (key > Arr[mid])
                 low = mid + 1;
             else
                 high = mid - 1;
         }
-        
+        Display(-1);
+        return -1; // unsuccessful
     }
 };
 
 int main() {
-    cout << "Hello World!\n";
+    int size = 7, low = 0, high = size - 1, key;
+    IterativeBinarySearch<int> mySearch(size);
+    cout << "Enter Key to be Searched : ";
+    cin >> key;
+    mySearch.BinarySearch(low, high, key);
+
+    return 0;
 }
