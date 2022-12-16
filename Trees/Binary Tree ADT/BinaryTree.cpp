1c81e9aefefc3a1e3405059652b40e7823beccd8
Razi Ullah <raziullah913@gmail.com>
2022-12-16 19:00:00 +0500

    structure for binary tree

---
+++ Trees/Binary Tree ADT/BinaryTree.cpp
@@ -0,0 +1,8 @@
+#include "BinaryTree.h"
+#include <iostream>
+
+// constructor
+template <class T>
+BinaryTree<T>::BinaryTree() {
+	root = NULL;
+}
---
+++ Trees/Binary Tree ADT/BinaryTree.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Node.h"
+
+template <class T>
+class BinaryTree {
+public:
+	Node<T>* root;
+	BinaryTree();
+	~BinaryTree();
+	Node<T>* newNode(T);
+	void InOrder(Node<T>*);
+	void PreOrder(Node<T>*);
+	void PostOrder(Node<T>*);
+	void IterativeInOrder(Node<T>*);
+	void IterativePreOrder(Node<T>*);
+	int Height(Node<T>*);
+	int CountNodes(Node<T>*);
+};
