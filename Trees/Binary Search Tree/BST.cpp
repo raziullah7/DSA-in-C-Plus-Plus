18057be369f6c362d4e5a254c7d9e2253c569102
Razi Ullah <raziullah913@gmail.com>
2022-12-26 19:22:08 +0500

    implemented Insert and Inorder functions

--- Trees/Binary Search Tree/BST.cpp
+++ Trees/Binary Search Tree/BST.cpp
@@ -1,5 +1,6 @@
 #include "BST.h"
 #include <iostream>
+using namespace std;
 
 // constructor
 BST::BST() {
@@ -39,7 +40,6 @@ Node* BST::RecursiveSearch(int key, Node* node) {
 	if (node == NULL) {
 		return NULL;
 	}
-
 	// successful search
 	if (node->data == key) {
 		return node;
@@ -51,5 +51,52 @@ Node* BST::RecursiveSearch(int key, Node* node) {
 	// going right
 	else {
 		return RecursiveSearch(key, node->right);
+	}
+}
+
+void BST::Insert(int key) {
+	// using R for traversing and r to store
+	// the parent of R, p is new node to be entered
+	Node* R = root, * p , * r;
+	// inserting at root position
+	if (root == NULL) {
+		p = new Node;
+		p->data = key;
+		p->left = NULL;
+		p->right = NULL;
+		root = p;
+		return;
+	}
+
+	// searching
+	while (R != NULL) {
+		r = R;
+		// going left
+		if (key < R->data)
+			R = R->left;
+		// going right
+		else if (key > R->data)
+			R = R->right;
+		// search was successful, so not inserting node
+		else
+			return;
+	}
+	// creating node
+	p = new Node;
+	p->data = key;
+	p->left = NULL;
+	p->right = NULL;
+	// inserting
+	if (p->data < r->data)
+		r->left = p;
+	else
+		r->right = p;
+}
+
+void BST::Inorder(Node* p) {
+	if (p != NULL) {
+		Inorder(p->left);
+		cout << p->data << " ";
+		Inorder(p->right);
 	}
 }
--- Trees/Binary Search Tree/Main.cpp
+++ Trees/Binary Search Tree/Main.cpp
@@ -1,7 +1,23 @@
 #include "BST.cpp"
-using namespace std;
 
 int main() {
 	BST myTree;
+	myTree.Insert(20);
+	myTree.Insert(15);
+	myTree.Insert(25);
+	myTree.Insert(10);
+	myTree.Insert(18);
+	myTree.Insert(23);
+	myTree.Insert(30);
+	myTree.Insert(5);
+	myTree.Insert(16);
+	myTree.Insert(22);
+	myTree.Insert(24);
+	myTree.Insert(40);
+	myTree.Insert(3);
+	myTree.Insert(35);
+
+	myTree.Inorder(myTree.GetRootElement());
+
 	return 0;
 }
