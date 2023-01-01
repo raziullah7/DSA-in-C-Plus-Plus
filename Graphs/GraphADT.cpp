6a403da9a503b64c7a3de6b7a6e711390a0af8af
Razi Ullah <raziullah913@gmail.com>
2023-01-01 17:20:53 +0500

    implemented constructor and destructor for Graph

--- Graphs/GraphADT.cpp
+++ Graphs/GraphADT.cpp
@@ -6,4 +6,26 @@ using namespace std;
 myGraph::myGraph(int vertices, int edges) {
     this->n = vertices;
     this->e = edges;
+    // (n+1) because arrays are 0 indexed
+    this->arr = new int*[n + 1];
+
+    // making the adjacency matrix, while not
+    // utilizing 0 index memory
+    for (int i = 1; i <= n; i++) {
+        arr[i] = new int[n + 1];
+    }
+
+    // filling up every location with 0, where the
+    // 0 index of every nested array is also not
+    // utilized for the sake of making it a matrix
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            arr[i][j] = 0;
+        }
+    }
+}
+
+// destructor
+myGraph::~myGraph() {
+    delete [] arr;
 }
--- Graphs/main.cpp
+++ Graphs/main.cpp
@@ -1,6 +1,9 @@
 #include "GraphADT.cpp"
 
 int main() {
+    // square shaped graph with 4 vertices
+    // and 4 edges
+    myGraph graph(4, 4);
     cout << "Hello, World!" << endl;
     return 0;
 }
