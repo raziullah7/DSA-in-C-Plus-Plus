3b12480a22f8444fe7cc14dbbea0a75494185830
Razi Ullah <raziullah913@gmail.com>
2023-01-05 00:09:11 +0500

    implemented enqueue and dequeue methods

--- Queue/Queue ADT (Array)/QueueADT.cpp
+++ Queue/Queue ADT (Array)/QueueADT.cpp
@@ -1,5 +1,6 @@
 #include "QueueADT.h"
 #include <iostream>
+using namespace std;
 
 // constructor
 template <class T>
@@ -17,3 +18,26 @@ Queue<T>::~Queue() {
     delete [] Q;
 }
 
+// enqueue method to insert elements from rear pointer
+template <class T>
+void Queue<T>::EnQueue(T element) {
+    if (rear == size-1) {
+        cout << "Queue is Empty!\n";
+        return;
+    }
+    rear++;
+    Q[rear] = element;
+    len++;
+}
+
+// dequeue method to delete elements from front pointer
+template <class T>
+void Queue<T>::DeQueue(T& element) {
+    if (rear == front) {
+        cout << "Queue is Full!\n";
+        return;
+    }
+    front++;
+    element = Q[front];
+    len--;
+}
