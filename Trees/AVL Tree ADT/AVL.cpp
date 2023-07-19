4efd484ceb65811ba836fb78aeb049edb9a3acd7
Razi Ullah <raziullah913@gmail.com>
2023-07-19 19:42:04 +0500

    implemented RR and LR rotations for AVL tree

--- Trees/AVL Tree ADT/AVL.cpp
+++ Trees/AVL Tree ADT/AVL.cpp
@@ -91,12 +91,21 @@ Node* AVL::Insert(Node* p, int key)
     // as the key was not NULL, we need to find height
     p->height = NodeHeight(p);
 
-    // not to check the case of LLRotation
+    // check the case of LLRotation
     if (BalanceFactor(p) == 2 && BalanceFactor(p->lChild) == 1)
     {
         return LLRotation(p);
     }
-
+    // check the case of LLRotation
+    else if (BalanceFactor(p) == -2 && BalanceFactor(p->rChild) == -1)
+    {
+        return RRRotation(p);
+    }
+    // check the case of LRRotation
+    else if (BalanceFactor(p) == 2 && BalanceFactor(p->lChild) == -1)
+    {
+        return LRRotation(p);
+    }
     // otherwise simply return p
     return p;
 
@@ -114,9 +123,9 @@ Node* AVL::LLRotation(Node* p)
     // right child of pl becomes left child of p
     p->lChild = plr;
 
-    // reevaluate height of p
+    // re-evaluate height of p
     p->height = NodeHeight(p);
-    //reevaluate height of pl
+    //re-evaluate height of pl
     pl->height = NodeHeight(pl);
 
     // the passed node p may be the root of the entire tree
@@ -127,5 +136,66 @@ Node* AVL::LLRotation(Node* p)
 
     // return the address of new root
     return pl;
+}
 
+Node* AVL::RRRotation(Node* p)
+{
+    // pr is right child of p
+    Node* pr = p->rChild;
+    // prl is the left child of pr
+    Node* prl = pr->lChild;
+
+    // performing rotation (pr becomes new root element)
+    pr->lChild = p;
+    // left child of pr becomes right child of p
+    p->rChild = prl;
+
+    // re-evaluate height of p
+    p->height = NodeHeight(p);
+    //re-evaluate height of pr
+    pr->height = NodeHeight(pr);
+
+    // in case the p is the root of the tree
+    if (root == p)
+    {
+        root = pr;
+    }
+
+    // return the address of new root
+    return pr;
+}
+
+Node* AVL::LRRotation(Node* p)
+{
+    // pl is left child of p
+    Node* pl = p->lChild;
+    // plr is the right child of pl
+    Node* plr = pl->rChild;
+
+    // performing 1st rotation (re-assigning plr's children)
+    // left child of plr becomes right child of pl
+    pl->rChild = plr->lChild;
+    // right child of plr becomes left child of p
+    p->lChild = plr->rChild;
+
+    // performing 2nd rotation (plr becomes new root element)
+    plr->rChild = p;
+    // pl becomes the left child of plr
+    plr->lChild = pl;
+
+    // re-evaluate height of p
+    p->height = NodeHeight(p);
+    //re-evaluate height of pl
+    pl->height = NodeHeight(pl);
+    //re-evaluate height of plr
+    plr->height = NodeHeight(plr);
+
+    // the passed node p may be the root of the entire tree
+    if (root == p)
+    {
+        root = plr;
+    }
+
+    // return the address of new root
+    return plr;
 }
--- Trees/AVL Tree ADT/Main.cpp
+++ Trees/AVL Tree ADT/Main.cpp
@@ -2,12 +2,23 @@
 
 int main()
 {
-    AVL treeLL;
-    treeLL.root = treeLL.Insert(treeLL.GetRoot(), 50);
-    treeLL.root = treeLL.Insert(treeLL.GetRoot(), 40);
-    treeLL.root = treeLL.Insert(treeLL.GetRoot(), 20);
+    // AVL treeLL;
+    // treeLL.root = treeLL.Insert(treeLL.GetRoot(), 50);
+    // treeLL.root = treeLL.Insert(treeLL.GetRoot(), 40);
+    // treeLL.root = treeLL.Insert(treeLL.GetRoot(), 20);
+    // treeLL.InOrder();
 
-    treeLL.InOrder();
+    // AVL treeRR;
+    // treeRR.root = treeRR.Insert(treeRR.GetRoot(), 5);
+    // treeRR.root = treeRR.Insert(treeRR.GetRoot(), 10);
+    // treeRR.root = treeRR.Insert(treeRR.GetRoot(), 20);
+    // treeRR.InOrder();
+
+    AVL treeLR;
+    treeLR.root = treeLR.Insert(treeLR.GetRoot(), 40);
+    treeLR.root = treeLR.Insert(treeLR.GetRoot(), 25);
+    treeLR.root = treeLR.Insert(treeLR.GetRoot(), 30);
+    treeLR.InOrder();
 
     return 0;
 }
